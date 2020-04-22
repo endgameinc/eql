ea7d9b985f1cfb003eab83e8352e05c6bf327ccd
Ross Wolf <31489089+rw-access@users.noreply.github.com>
2020-04-22 14:24:46 -0600

    Add tests

--- tests/test_nulls.py
+++ tests/test_nulls.py
@@ -1,14 +1,14 @@
 """Test Python Engine for EQL."""
 import unittest
 
-from eql.ast import Literal, Not, Null
+from eql.ast import Literal
 from eql.engine import PythonEngine
 from eql.parser import parse_expression, skip_optimizations
 
 from . import utils
 
 
-class TestPythonEngine(unittest.TestCase):
+class TestNullPropagation(unittest.TestCase):
     """Test correctness of null handling through the optimizer and Python Engine."""
 
     @staticmethod
@@ -16,86 +16,81 @@ class TestPythonEngine(unittest.TestCase):
         """Evaluate an unoptimized expression in the ``PythonEngine``."""
         return PythonEngine().convert(expr)(None)
 
+    def assert_fold(self, text, output):
+        """Helper method for checking that expressions fold and evaluate correctly."""
+        with skip_optimizations:
+            parsed = parse_expression(text)
+
+        self.assertNotIsInstance(parsed, Literal)
+        self.assertEqual(self.engine_eval(parsed), output, "for: {} == {}".format(text, output))
+        self.assertEqual(parsed.fold(), output, "for: {} == {}".format(text, output))
+
     def test_null_or_handling(self):
         """Test that nulls are correctly propagated through ``or`` for the engine and optimizer."""
-        engine = PythonEngine()
+        expected = [
+            ("true  or false", True),
+            ("true  or null ", True),
+            ("true  or true ", True),
+            ("false or null ", None),
+            ("false or false", False),
+            ("false or true ", True),
+            ("null  or false", None),
+            ("null  or null ", None),
+            ("null  or true ", True),
+        ]
 
-        def engine_eval(expr):
-            return engine.convert(expr)(None)
-
-        with skip_optimizations:
-            expected = [
-                ("true  or false", True),
-                ("true  or null ", True),
-                ("true  or true ", True),
-                ("false or null ", None),
-                ("false or false", False),
-                ("false or true ", True),
-                ("null  or false", None),
-                ("null  or null ", None),
-                ("null  or true ", True),
-            ]
-
-            for text, output in expected:
-                parsed = parse_expression(text)
-                self.assertNotIsInstance(parsed,  Literal)
-                self.assertEqual(engine_eval(parsed), output, "for: {} == {}".format(text, output))
-                self.assertEqual(parsed.fold(), output, "for: {} == {}".format(text, output))
+        for text, output in expected:
+            self.assert_fold(text, output)
 
     def test_null_and_handling(self):
         """Test that nulls are correctly propagated through ``and``."""
-        with skip_optimizations:
-            expected = [
-                ("true  and false",  False),
-                ("true  and null ", None),
-                ("true  and true ", True),
-                ("false and null ", False),
-                ("false and false", False),
-                ("false and true ", False),
-                ("null  and false", False),
-                ("null  and null ", None),
-                ("null  and true ", None),
-            ]
+        expected = [
+            ("true  and false",  False),
+            ("true  and null ", None),
+            ("true  and true ", True),
+            ("false and null ", False),
+            ("false and false", False),
+            ("false and true ", False),
+            ("null  and false", False),
+            ("null  and null ", None),
+            ("null  and true ", None),
+        ]
 
-            for text, output in expected:
-                parsed = parse_expression(text)
-                self.assertNotIsInstance(parsed,  Literal)
-                self.assertEqual(self.engine_eval(parsed), output, "for: {} == {}".format(text, output))
-                self.assertEqual(parsed.fold(), output, "for: {} == {}".format(text, output))
+        for text, output in expected:
+            self.assert_fold(text, output)
 
     def test_null_compares(self):
         """Test that all types can be compared to null."""
         values = [None, utils.random_bool(), utils.random_int(), utils.random_string(), utils.random_float()]
 
-        with skip_optimizations:
-            for v in values:
-                unparsed = utils.unfold(v)
+        for v in values:
+            unparsed = utils.unfold(v)
 
-                def folds_to(fmt_string, expected):
-                    parsed = parse_expression(fmt_string.format(unparsed))
-                    self.assertNotIsInstance(parsed, Literal)
-                    self.assertEqual(parsed.fold(), expected)
-                    self.assertEqual(self.engine_eval(parsed), expected)
+            def folds_to(fmt_string, expected):
+                self.assert_fold(fmt_string.format(unparsed), expected)
 
-                folds_to("{}   == null", v is None)
-                folds_to("{}   != null", v is not None)
-                folds_to("null ==   {}", v is None)
-                folds_to("null !=   {}", v is not None)
+            folds_to("{}   == null", v is None)
+            folds_to("{}   != null", v is not None)
+            folds_to("null ==   {}", v is None)
+            folds_to("null !=   {}", v is not None)
 
-                if not isinstance(v, bool):
-                    folds_to("{} <    null", None)
-                    folds_to("{}   <= null", None)
-                    folds_to("{}   >  null", None)
-                    folds_to("{}   >= null", None)
-                    folds_to("null <    {}", None)
-                    folds_to("null <=   {}", None)
-                    folds_to("null >=   {}", None)
-                    folds_to("null >    {}", None)
+            if not isinstance(v, bool):
+                folds_to("{} <    null", None)
+                folds_to("{}   <= null", None)
+                folds_to("{}   >  null", None)
+                folds_to("{}   >= null", None)
+                folds_to("null <    {}", None)
+                folds_to("null <=   {}", None)
+                folds_to("null >=   {}", None)
+                folds_to("null >    {}", None)
 
     def test_not_null(self):
         """Test that ``not null`` returns ``null``."""
-        with skip_optimizations:
-            parsed = parse_expression("not null")
-            self.assertEqual(parsed, Not(Null()))
-            self.assertEqual(parsed.fold(), None)
-            self.assertEqual(self.engine_eval(parsed), None)
+        expected = [
+            ("not true", False),
+            ("not false", True),
+            ("not null", None),
+        ]
+
+        for text, output in expected:
+            self.assert_fold(text, output)
